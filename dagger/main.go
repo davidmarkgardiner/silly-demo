0b69ec352a3c7620791bcf9affe24c1b57220c77
Trigsy1 <davidmarkgardiner@gmail.com>
2024-01-07 15:49:07 +0000

    adding davidmarkgardiner ghcr

--- dagger/main.go
+++ dagger/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 var ctx = context.Background()
-var image = "ghcr.io/vfarcic/silly-demo"
+var image = "ghcr.io/davidmarkgardiner/silly-demo"
 var dev = false
 
 const (
@@ -78,7 +78,7 @@ func publishImages(client *dagger.Client, dockerfile string, tags []string) {
 		if !dev && !signed {
 			cosignCmd := fmt.Sprintf("cosign sign --yes --key env://COSIGN_PRIVATE_KEY %s", imageAddr)
 			if len(os.Getenv("REGISTRY_PASSWORD")) > 0 {
-				cosignCmd = fmt.Sprintf("cosign login ghcr.io --username vfarcic --password $REGISTRY_PASSWORD && %s", cosignCmd)
+				cosignCmd = fmt.Sprintf("cosign login ghcr.io --username davidmarkgardiner --password $REGISTRY_PASSWORD && %s", cosignCmd)
 			}
 			output, err := client.Container().
 				From("bitnami/cosign:2.2.1").
@@ -158,7 +158,7 @@ func publishTimoni(client *dagger.Client, tag string) {
 			WithExec([]string{"go", "install", "github.com/stefanprodan/timoni/cmd/timoni@latest"}).
 			WithDirectory("timoni", client.Host().Directory("timoni")).
 			WithSecretVariable("REGISTRY_PASSWORD", regPass).
-			WithExec([]string{"sh", "-c", fmt.Sprintf(`timoni mod push timoni oci://%s-package --version %s --creds vfarcic:$REGISTRY_PASSWORD`, image, tag)}).
+			WithExec([]string{"sh", "-c", fmt.Sprintf(`timoni mod push timoni oci://%s-package --version %s --creds davidmarkgardiner:$REGISTRY_PASSWORD`, image, tag)}).
 			Stdout(ctx)
 		if err != nil {
 			println(out)
